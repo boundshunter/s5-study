970521bd15f77bd70987c395b68a63c693103e65
sujunfeng <76686094@qq.com>
2017-11-23 09:48:44 +0800

    Default

--- s4-1122/func.py
+++ s4-1122/func.py
@@ -1,3 +1,39 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 # Author:summer_han
+
+#面向对象  类 --> class
+
+
+#面向过程   过程 ---> def
+#函数式变成  函数 ---> def
+
+
+def test(x):
+    "文档描述"
+    x+=1
+    return x
+
+#定义函数
+def func1():
+    "testing"
+    print("in the func1")
+    return 0
+
+#定义过程
+def func2():
+    "testing2"
+    print("in the func2")
+
+#函数和过程的区别，过程就是没有返回值的函数
+
+x = func1()
+y = func2()
+print("From func1 retrun: %s" % x)
+print("From func2 return: %s" % y)
+
+#结果来看，过程隐式定义了返回值，所以在python中，过程和函数区别不大
+
+#为什么要使用函数：
+
+
--- s4-1122/func_return.py
+++ s4-1122/func_return.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 # Author:summer_han
+'''
+return 返回值 ，结束函数
+return 可以返回什么样的值，
+1、个数，类型， 值都不固定，随意指定
+2、
+'''
+
+def test1():
+    print("in the test1")
+
+def test2():
+    print("in the test2")
+    return 0
+
+def test3():
+    print("in the test3")
+    return 1,"hello",['xi','han'],{"dabao":"erbao"}
+
+x=test1()
+y=test2()
+z=test3()
+
+print(type(x),x)  # 返回值=0 返回none 不定义return 隐式返回 None
+print(type(y),y)  #返回值=1 返回 object
+print(type(z),z)  #返回值=2 返回元组 tuple 全都包含在一个元组中返回,其实是返回一个值
+
+#为什么要有返回值，它的作用是干嘛 ,后面其他程序逻辑，需要返回值结果进行操作
+

33feb4f8a4280f413455ae2437ab7b54eafb3ffe
MACHENIKE <76686094@qq.com>
2017-12-04 20:33:59 +0800

    新的func

--- 作业/工资管理系统/工资管理系统-作业.py
+++ 作业/工资管理系统/工资管理系统-作业.py
@@ -37,13 +37,13 @@ def alter_staff_info(username,salary): # 修改员工姓名函数
         f_alter.write(str(info))
     print("修改成功")
 
-def add_staff_info(username,salary):
+def add_staff_info(username,salary):   # 增加用户信息函数
     info.setdefault(username,salary) # 增加一个字典值
     with open(DB_DIR,'w',encoding="utf-8") as f_add:
         f_add.write(str(info))
     print("增加成功")
 
-def exit_system():
+def exit_system(): # 退出函数
     print("退出程序")
     sys.exit()
 
@@ -51,59 +51,59 @@ def exit_system():
 def login(): # 定义主体函数
     request_list()  # 调用显示功能列表函数
     while True:
-        choice = input(">>:")
-        if choice == '1':
+        choice = input(">>:")   # 获取输入 选项
+        if choice == '1':   # 如果选择 1
             exit_flag = False
             while not exit_flag:
-                username = input("输入要查询的员工姓名（例如Alex）:")
-                if username in info:
-                    select_staff_info(username)
+                username = input("\033[31;1m输入要查询的员工姓名（例如Alex）:\033[0m")  # 输入要查询的员工名
+                if username in info:  # 如果员工存在
+                    select_staff_info(username)  # 调用查询函数
                     print("\r")
-                    login()
+                    login()  # 返回程序入口
                     continue
                 else:
-                    print("您的用户名输入错误，请重新输入:")
+                    print("\033[41;1m您的用户名输入错误，请重新输入:\033[0m")  # 员工不存在，提示重新输入
                     continue
-        elif choice == '2':
+        elif choice == '2':  # 选择 修改员工记录
             while True:
-                count = input("请输入要修改的员工姓名和工资，用空格分隔(例如：Alex 10) : ").split()
-                if len(count) == 2:
+                input_args_count = input("\033[31;1m请输入要修改的员工姓名和工资，用空格分隔(例如：Alex 10) : \033[0m").split()  # 获取参数
+                if len(input_args_count) == 2:  # 判断获取参数个数
                     #username,salary = input("请输入要修改的员工姓名和工资，用空格分隔(例如：Alex 10) : ").split()
-                    username = count[0]
-                    salary = count[1]
-                    if username in info:
-                        alter_staff_info(username,salary)
+                    username = count[0]  # 用户名赋值
+                    salary = count[1]  # 工资 赋值
+                    if username in info:  # 判断用户名存在
+                        alter_staff_info(username,salary)  # 调用 修改用户函数
                         print("\r")
-                        login()
+                        login()  # 返回系统头部
                     else:
-                        print("您的用户不存在，重新输入！")
+                        print("\033[41;1m您的用户不存在，重新输入！\033[0m")  # 判断用户不存在
                         continue
                 else:
-                    print("您输入的参数不对，请重新输入!")
+                    print("\033[41;1m您输入的参数不对，请重新输入!\033[0m")  # 判断参数个数不正确
                     continue
 
-        elif choice == '3':
+        elif choice == '3':  # 选择 增加新员工记录
             while True:
-                input_args = input("请输入要增加的员工姓名，工资，用空格分隔符(例如：Eric 100000) :").split()
-                if len(input_args) == 2:
-                    username = input_args[0]
+                input_args = input("\033[31;1m请输入要增加的员工姓名，工资，用空格分隔符(例如：Eric 100000) :\033[0m").split()   # 获取参数
+                if len(input_args) == 2:  # 判断参数个数
+                    username = input_args[0]  # 赋值
                     salary = input_args[1]
                     # print(username)
                     # print(salary)
-                    if username not in info:
-                        add_staff_info(username,salary)
-                        login()
+                    if username not in info:  # 判断用户是否存在，如果不存在
+                        add_staff_info(username,salary)  # 调用增加用户函数
+                        login()  # 返回系统头部
                     else:
-                        print("您输入的用户存在，请使用其他用户名!")
+                        print("\033[41;1m您输入的用户存在，请使用其他用户名!\033[0m")  # 用户名存在，重新输入
                         continue
                 else:
-                    print("您输入参数个数有误，请重新输入！")
+                    print("\033[41;1m您输入参数个数有误，请重新输入！\033[0m")  # 参数个数错误，重新输入
                     continue
-        elif choice == '4':
+        elif choice == '4':  # 退出函数
             exit_system()
 
         else:
-            print("您的选择有误，请重新选择")
+            print("\033[41;1m您的选择有误，请重新选择\033[0m")   # 选择错误 重新选择
             login()
 
 if __name__ == '__main__':
